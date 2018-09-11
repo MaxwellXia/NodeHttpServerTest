a990a6a455bf2d28ed6ebcd5dc85e77991190938
xiawm <xiawm@ldomotors.com>
2018-09-11 15:36:32 +0800

    完成了文件的下载测试

--- ExpressApp1/Client/ClientGetFrame.js
+++ ExpressApp1/Client/ClientGetFrame.js
@@ -1,3 +1,41 @@
+var fs = require('fs');
+
 exports.GetFrame = function (req, res)
 {
+    var url = req.body['URL'];
+
+    var fileStat = fs.stat(url,
+        function (err, stats) {
+            if (err) {
+                response.writeHead(404);
+                response.end();
+                return null;
+            }
+            //TODO:Content-Type应该根据文件类型设置
+            res.writeHead(200, { "Content-Type": "text/plain", "Content-Length": stats.size });
+
+            //使用Stream
+            var stream = fs.createReadStream(url);
+            var count = 0;
+
+            stream.on('data', function (chunk) {
+                //console.log(chunk);
+                console.log("请求数据次数：" + (++count).toString());
+                console.log(chunk.length);
+                res.write(chunk);
+            });
+
+            stream.on('end', function () {
+                count = 0;
+                console.log("结束");
+                res.end();
+            });
+
+            stream.on('error', function () {
+                count = 0;
+                console.log("错误");
+                res.end();
+            });
+        }
+    );
 }
